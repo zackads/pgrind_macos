0391c5358cdc12fad191863e055ccb5317e9c6d4
Zack Adlington <zackads@users.noreply.github.com>
2026-01-20 15:14:33 +0000

    Initial Commit

---
+++ pgrind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pgrind
+//
+//  Created by Zack Adlington on 20/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pgrind/pgrindApp.swift
@@ -0,0 +1,17 @@
+//
+//  pgrindApp.swift
+//  pgrind
+//
+//  Created by Zack Adlington on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct pgrindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
