37f36be72d498ab7e0bb7e1e0ce280c45aaa0907
Zack Adlington <zackads@users.noreply.github.com>
2026-05-15 08:24:39 +0100

    Remove WebpageProblem
    Want to focus on ImageProblems.

--- pgrind/Migrations/PgrindMigrationPlan.swift
+++ pgrind/Migrations/PgrindMigrationPlan.swift
@@ -5,16 +5,29 @@
 //  Created by Zack Adlington on 21/02/2026.
 //
 
+import Foundation
 import SwiftData
 
 enum MigrationPlan: SchemaMigrationPlan {
     static var schemas: [any VersionedSchema.Type] {[
-        SchemaV1.self, // SchemaV2.self
+        SchemaV1.self,
+        SchemaV2.self,
     ]}
 
     static var stages: [MigrationStage] {
-        [
-//            .lightweight(fromVersion: SchemaV1.self, toVersion: SchemaV2.self),
-        ]
+        [migrateV1toV2]
     }
+
+    static let migrateV1toV2 = MigrationStage.custom(
+        fromVersion: SchemaV1.self,
+        toVersion: SchemaV2.self,
+        willMigrate: { context in
+            let webpageProblems = try context.fetch(FetchDescriptor<WebpageProblem>())
+            for problem in webpageProblems {
+                context.delete(problem)
+            }
+            try context.save()
+        },
+        didMigrate: nil
+    )
 }
---
+++ pgrind/Migrations/SchemaV2.swift
@@ -0,0 +1,21 @@
+//
+//  SchemaV2.swift
+//  pgrind
+//
+//  Removes WebpageProblem from the schema.
+//
+
+import Foundation
+import SwiftData
+
+enum SchemaV2: VersionedSchema {
+    static var versionIdentifier = Schema.Version(2, 0, 0)
+    static var models: [any PersistentModel.Type] { [
+        Attempt.self,
+        Course.self,
+        ImageProblem.self,
+        Problem.self,
+        ProblemSet.self,
+        ScreenshotItem.self,
+    ] }
+}
